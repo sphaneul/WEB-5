adcc69583deb5af540a5812ff5119fb9eac7e68c
sphaneul <63492821+sphaneul@users.noreply.github.com>
2020-11-18 23:47:56 +0900

    Update check.php

--- check.php
+++ check.php
@@ -4,13 +4,13 @@ $PW=$_POST['PW'];
 $con=mysqli_connect("localhost","root","kitty0809","mydb");
 //mysql연결
 
-$query="SELECT * FROM member;";
+$query="SELECT * FROM member;"; //읽어오기
 $result=mysqli_query($con, $query);
 if($result->num_rows==1){
   $row=$result->fetch_array(MYSQLI_ASSOC);
-  if($row['userpw']==$pw){
+  if($row['userpw']==$pw){ //
     $_SESSION['userid']=$id;
-    if(isset($_SESSION['userid'])){
+    if(isset($_SESSION['userid'])){ //세션이 있으면 index.php로 이동(최종 완료 되었을 때)
       header('location: index.php')
     }
     else{
